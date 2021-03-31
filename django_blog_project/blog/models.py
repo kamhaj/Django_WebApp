d9636eb52658c91453db79cbbe8fc2c42cc9f0ac
Kamil <ckkomancz18@yahoo.com>
2021-03-31 15:14:56 +0200

    add Post class (models.py) and register it in admin panel (admin.py)

--- django_blog_project/blog/models.py
+++ django_blog_project/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+## we need users, posts.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()        # unrestricted text
+    date_posted = models.DateTimeField(default=timezone.now)  #its a function now(), but we dont want to execute it right away??
+    author = models.ForeignKey(User, on_delete=models.CASCADE) ## if user was deleted, deleted his/her posts as well
+
+
+    def __str__(self):
+        ## return it in a way you want it to be printed out
+        return self.title
+
